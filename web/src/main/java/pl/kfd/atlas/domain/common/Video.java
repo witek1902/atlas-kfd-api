31033a31615262610cc0a11e26f4043a5429b6b4
witek1902 <witek1902@gmail.com>
2018-03-01 21:07:25 +0100

    Add Video URL to embed converter.

--- web/src/main/java/pl/kfd/atlas/domain/common/Video.java
+++ web/src/main/java/pl/kfd/atlas/domain/common/Video.java
@@ -1,15 +1,33 @@
 package pl.kfd.atlas.domain.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
 public class Video implements Serializable {
 
+    private final static String YOUTUBE_URL = "youtube";
+    private final static String YOUTUBE_WATCH_PREFIX = "watch?v=";
+    private final static String YOUTUBE_EMBED_PREFIX = "embed/";
+
     private final String url;
+    private final String embedYoutubeUrl;
+
+    public Video(String videoUrl) {
+        url = videoUrl;
+        embedYoutubeUrl = convertYoutubeToEmbedUrl(videoUrl);
+    }
+
+    private String convertYoutubeToEmbedUrl(String url) {
+        if (url == null)
+            return "";
+
+        if(!url.contains(YOUTUBE_URL))
+            return url;
+
+        return url.replace(YOUTUBE_WATCH_PREFIX, YOUTUBE_EMBED_PREFIX);
+    }
 
     public static Video empty() {
         return new Video("");
