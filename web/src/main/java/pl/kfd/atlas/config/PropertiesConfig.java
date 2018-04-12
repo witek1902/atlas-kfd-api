dc783a55de06e1cf0f51f9b9b7a274aad1c87ac2
witek1902 <witek1902@gmail.com>
2018-04-12 21:39:34 +0200

    Remove unecessary classes.

--- web/src/main/java/pl/kfd/atlas/config/PropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.kfd.atlas.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "api.properties")
-public class PropertiesConfig {
-}
